90c870d814c5f22a2b0c109e6354d45c986c02d4
Tal Yaron <tal.yaron@gmail.com>
2016-08-07 14:29:05 +0300

    options

---
+++ options.js
@@ -0,0 +1,14 @@
+<!doctype html>
+   <html>
+   <head>
+   <title>Mithril</title>
+   <script src="mithril.js"></script>
+      </head>
+
+
+      <body>
+      <script>
+
+      </script>
+      </body>
+      </html>
